249974adcb1889cda270417e68c6e125e558cd55
Fallenstar <ringanaro@gmail.com>
2024-10-14 18:06:32 +0300

    Add stylelint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import globals from "globals";
-import process from "process";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
@@ -9,17 +8,13 @@ export default [
     ignores: ["dist"],
   },
   { files: ["src/**/*.{js,ts,d.ts}"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
   {
     rules: {
-      "linebreak-style": [
-        "error",
-        require("os").EOL === "\r\n" ? "windows" : "unix",
-      ],
+      "linebreak-style": process.env.NODE_ENV === "prod" ? "unix" : "windows",
     },
   },
 ];
