0541776b88f7d9336c1fea1c6790d37940f3e639
Fallenstar <ringanaro@gmail.com>
2024-10-14 11:48:30 +0300

    Add semicolons

--- src/apiService.ts
+++ src/apiService.ts
@@ -12,7 +12,7 @@ export class ApiService {
             } else if (request.readyState === 4) {
                 callback(`Wasn't able to load target [${target}].`, null);
             }
-        })
+        });
 
         request.open("GET", `${this._baseApiUrl}/${target}`);
         request.send();
@@ -28,7 +28,7 @@ export class ApiService {
                 } else if (request.readyState === 4) {
                     reject(`Wasn't able to load target [${target}].`);
                 }
-            })
+            });
 
             request.open("GET", `${this._baseApiUrl}/${target}`);
             request.send();
@@ -36,9 +36,9 @@ export class ApiService {
     }
 
     async fetchHttpRequest(target: string): Promise<Character> {
-        const response = await fetch(`${this._baseApiUrl}/${target}`)
+        const response = await fetch(`${this._baseApiUrl}/${target}`);
         if (!response.ok)
-            throw new Error(`Wasn't able to load target [${target}].\n${response.statusText}`)
+            throw new Error(`Wasn't able to load target [${target}].\n${response.statusText}`);
         const char = await response.json() as Character;
         char.portraitUrl = `${this._baseApiUrl}/${target}/portrait`;
         return char;
