f89f4ee974cdfe6aa898ec4ecb75a2bfd4eb49f5
Fallenstar <ringanaro@gmail.com>
2024-10-14 15:30:35 +0300

    Add prettier and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default [
+  {
+    ignores: ["dist"],
+  },
+  { files: ["src/**/*.{js,ts,d.ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+];
